7a9874ef612c6ad3960a0d142d4f37472c4b4c49
Vaibhav Mande <vaibhavrmande@gmail.com>
2023-01-26 10:41:13 -0500

    add tailwind and prettier

---
+++ .prettierrc.js
@@ -0,0 +1,13 @@
+/** @type {import ('prettier').Options} */
+module.exports = {
+  useTabs: false,
+  singleAttributePerLine: false,
+  trailingComma: 'all',
+  arrowParens: 'always',
+  bracketSpacing: true,
+  jsxSingleQuote: false,
+  printWidth: 100,
+  semi: false,
+  singleQuote: true,
+  tabWidth: 2,
+}
